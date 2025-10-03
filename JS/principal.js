b5e1a3be58881309696fa15f33c488bd4a37447a
Nicolas J. <164082910+nicolassilveriojr@users.noreply.github.com>
2025-10-03 11:02:24 -0300

    123123

--- JS/principal.js
+++ JS/principal.js
@@ -3,5 +3,6 @@ console.log(titulo);
 console.log(titulo.textContent);
 titulo.textContent = ("Nickin Nutricionista")
 var paciente = document.querySelector("#primeiro-paciente");
-console.log(paciente)
-
+console.log(paciente);
+var tdPeso = paciente.querySelector(".info-peso");
+console.log(tdPeso); //
