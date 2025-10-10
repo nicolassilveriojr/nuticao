6ec12ad65d35333c88f4ddad0f71fe4689da629c
Nicolas J. <164082910+nicolassilveriojr@users.noreply.github.com>
2025-10-10 10:58:56 -0300

    123123

--- JS/principal.js
+++ JS/principal.js
@@ -1,49 +1,46 @@
-//console.log(document.querySelector("h1"));
 var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
-titulo.textContent = ("nickin nutritations");
-
-
-
+titulo.textContent=("White nutricao");
 
 var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
+var tdNome = documentquerySelector(".info-nome");
+var nome = tdNome.textContent;
+console.log(nome);
+
+var tdPeso = document.querySelector(".info-peso")
 var peso = tdPeso.textContent;
-console.log(paciente);
-console.log(tdPeso);
+console.log(peso);
 
-
-
-
-var tdAltura = document.querySelector("#primeiro-paciente");
-var altura = document.querySelector(".info-altura");
+var tdAltura = document.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 console.log(altura);
 
+var tdImc = document.querySelector(".info-imc");
+consolelog(imc);
 
 
+var pesoEhValido = true;
+var alturaEhValida = true;
 
-//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-var pesoehValido = true;
- var alturaehValido = true;
-
-
-if (peso <= 0 || peso >= 1000) {
-  console.log("peso inválido");
-  var pesoEhValido = false;
-  tdlmc.textContent = "peso invalido";
+if (peso < 0 || peso > 1000) {
+console.log("Peso inválido");
+pesoEhValido = false;
+tdImc.textContent = "Peso inválido!";
 }
 
-
-if(altura <= 0||altura >= 3.00){
-   console.log("Altura inválida");
-    var alturaEhValido = false;
-    tdlmc.textContent = "altura invalida!";
+if (altura > 3.00 || altura < 0) {
+console.log("Altura inválida");
+alturaEhValida = false;
+tdImc.textContent = "Altura inválida!";
 }
 
+if (alturaEhValida && pesoEhValido){
+var imc = peso / (altura * altura);
+tdImc.textContent = imc;
+}
 
- if(alturaEhValida&&pesoEhValido){
-   var imc = peso / (altura * altura) ;
-   tdImc.textContent = imc;
- }
+if (alturaEhValida && pesoEhValido){
+var imc = peso / (altura * altura);
+tdImc.textContent = imc;
+}
