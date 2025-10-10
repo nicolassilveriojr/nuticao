40def4109f92909e37756c8df4f4914717b11593
Nicolas J. <164082910+nicolassilveriojr@users.noreply.github.com>
2025-10-10 10:47:35 -0300

    123123

--- JS/principal.js
+++ JS/principal.js
@@ -2,7 +2,7 @@
 var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
-titulo.textContent = ("Pedro nutritations");
+titulo.textContent = ("nickin nutritations");
 
 
 
