66c5fe06384447a9cbaa9a173be197b5a7798ebb
Nicolas J. <164082910+nicolassilveriojr@users.noreply.github.com>
2025-10-10 09:38:54 -0300

    123123

--- JS/principal.js
+++ JS/principal.js
@@ -4,13 +4,27 @@ console.log(titulo);
 console.log(titulo.textContent);
 titulo.textContent = ("Nego nutritations");
 
+
+
+
 var paciente = document.querySelector("#primeiro-paciente");
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 console.log(paciente);
 console.log(tdPeso);
 
+
+
+
 var tdAltura = document.querySelector("#primeiro-paciente");
 var altura = document.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 console.log(altura);
+if (peso < 0 || peso > 1000) {
+    console.log("peso inválido");
+}
+
+
+if (altura, 0 || altur > 3.00) {
+    console.log("Altura inválida");
+}
