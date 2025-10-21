7cb73bf45d40eaf242c08ae2a9ddf052935fbad1
Nicolas J. <164082910+nicolassilveriojr@users.noreply.github.com>
2025-10-21 10:05:22 -0300

    123123

--- JS/principal.js
+++ JS/principal.js
@@ -1,44 +1,38 @@
 var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
-titulo.textContent = "nickin nutricao";
+titulo.textContent = ("nickin nutricao");
 
-var pacientes = document.querySelectorAll(".paciente");
+var pacientes = paciente.querySelectorAll(".paciente");
 
 for (var i = 0; i < pacientes.length; i++) {
    var paciente = pacientes[i];
 
-   var tdPeso = paciente.querySelector(".info-peso");
-   var peso = parseFloat(tdPeso.textContent);
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
 
-   var tdAltura = paciente.querySelector(".info-altura");
-   var altura = parseFloat(tdAltura.textContent);
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
 
-   var tdImc = paciente.querySelector(".info-imc");
+var tdImc = paciente.querySelector(".info-imc");
 
-   var pesoEhValido = true;
-   var alturaEhValida = true;
+var pesoEhValido = true;
+var alturaEhValida = true;
 
-   if (peso <= 0 || peso >= 1000) {
-      console.log("Peso inválido");
-      pesoEhValido = false;
-   }
-
-   if (altura <= 0 || altura >= 3.00) {
-      console.log("Altura inválida");
-      alturaEhValida = false;
-   }
-
-   if (!pesoEhValido) {
-      tdImc.textContent = "Peso inválido!";
-      paciente.classList.add("paciente-invalido");
-
-   } else if (!alturaEhValida) {
-      tdImc.textContent = "Altura inválida!";
-      paciente.classList.add("paciente-invalido");
-
-   } else {
-      var imc = peso / (altura * altura);
-      tdImc.textContent = imc.toFixed(2);
-   }
+if (peso <= 0 || peso <= 1000) {
+   console.log("Peso inválido");
+   pesoEhValido = false;
+   tdImc.textContent = "Peso inválido!";
 }
+
+if (altura <= 3.00 || altura <= 0) {
+   console.log("Altura inválida");
+   alturaEhValida = false;
+   tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhValida && pesoEhValido) {
+   var imc = peso / (altura * altura);
+   tdImc.textContent = imc;
+}
+};
